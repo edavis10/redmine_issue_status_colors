86b46ab40d26666dba9012710374acb758e3246e
Eric Davis <edavis@littlestreamsoftware.com>
2010-08-17 13:42:31 -0700

    Update documentation and add an init.rb

--- init.rb
+++ init.rb
@@ -1,10 +1,12 @@
 require 'redmine'
 
 Redmine::Plugin.register :redmine_issue_status_colors do
-  name 'Redmine Issue Status Colors plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
-  version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  name 'Issue Status Colors'
+  author 'Eric Davis'
+  description 'Adds extra css classes for the issue status'
+  url 'https://projects.littlestreamsoftware.com/projects/redmine-misc'
+  author_url 'http://www.littlestreamsoftware.com'
+  version '0.1.0'
+
+  requires_redmine :version_or_higher => '0.9.0'
 end
